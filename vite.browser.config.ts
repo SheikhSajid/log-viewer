71822659ffcd321c88557a4b9f945dc1b0515fe0
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2025-07-08 16:04:53 +1000

    Fix vite config

--- vite.browser.config.ts
+++ vite.browser.config.ts
@@ -1,25 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
-  root: '.',
   base: '/log-viewer',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'),
-    },
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
+  plugins: [react()]
 });
