3c9e1087a946ddeb7664fe30b4022551c8adf8b3
Sk Sajidul Kadir <sheikh.sajid522@gmail.com>
2025-07-08 15:35:13 +1000

    Updates

---
+++ assets/allPathsLoader-B-XfUQxk.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/allPaths-CQr-2wF3.js","assets/index-BKzgFDAn.js","assets/index-BvSuWPlB.js","assets/index-tTV20nd-.js","assets/index-D-w1gv_z.css"])))=>i.map(i=>d[i]);
+import{_ as o,a as n,b as i}from"./index-tTV20nd-.js";var _=function(e,a){return o(void 0,void 0,void 0,function(){var t;return n(this,function(r){switch(r.label){case 0:return[4,i(()=>import("./allPaths-CQr-2wF3.js"),__vite__mapDeps([0,1,2,3,4]))];case 1:return t=r.sent().getIconPaths,[2,t(e,a)]}})})};export{_ as allPathsLoader};
